84e4a9eb10c98f9cd1f14612ec753533c80e5fff
Donthula-Harika <harika.donthula2010@gmail.com>
2026-04-15 00:02:04 +0530

    Updated API base URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+// const API_BASE = 'http://localhost:5000/api';
+const API_BASE = "https://ai-doubt-solver-backend.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
